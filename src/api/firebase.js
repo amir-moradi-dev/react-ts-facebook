6ab35d824bfdc6cbaa4f4c5592186b37ed117070
Amir_Moradi <amir.moradi.dev@gmail.com>
2023-02-13 23:20:51 +0330

    Edited Style ...

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAWz_nRCLN1GUKMOplLjMHHmHCNY1sP3Ag",
-    authDomain: "react-ts-facebook.firebaseapp.com",
-    projectId: "react-ts-facebook",
-    storageBucket: "react-ts-facebook.appspot.com",
-    messagingSenderId: "1060663110401",
-    appId: "1:1060663110401:web:bf98412bb04ba12aa50900",
-    measurementId: "G-GD69XL2SHZ"
+  apiKey: "AIzaSyAWz_nRCLN1GUKMOplLjMHHmHCNY1sP3Ag",
+  authDomain: "react-ts-facebook.firebaseapp.com",
+  projectId: "react-ts-facebook",
+  storageBucket: "react-ts-facebook.appspot.com",
+  messagingSenderId: "1060663110401",
+  appId: "1:1060663110401:web:bf98412bb04ba12aa50900",
+  measurementId: "G-GD69XL2SHZ",
 };
 
 // Initialize Firebase
